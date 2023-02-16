ba773138bb0d700e0f9116c33ea096307fa098d2
Aaron Phan <aarn.phan@gmail.com>
2023-02-17 00:36:06 +1100

    First setup project

---
+++ src/main/java/com/example/gunplaretail/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.gunplaretail;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
